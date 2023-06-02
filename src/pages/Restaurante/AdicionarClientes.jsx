407e1db4b9d187c27251f1ad63868c27af38fdd5
Kievv <ievsouza622@gmail.com>
2023-06-02 17:00:47 -0300

    Valor agora é mostrado, na hora de calcular valor a pagar

--- src/pages/Restaurante/AdicionarClientes.jsx
+++ src/pages/Restaurante/AdicionarClientes.jsx
@@ -1,17 +1,31 @@
 import { useState } from 'react';
 
-const AdicionarClientes = ({ onAdicionarCliente }) => {
+const AdicionarClientes = ({ onAdicionarCliente, produtos, handleProdutoCheckboxChange }) => {
   const [cliente, setCliente] = useState('');
+  const [produtosSelecionados, setProdutosSelecionados] = useState([]);
 
   const handleAdicionarCliente = () => {
     if (cliente.trim() !== '') {
-      onAdicionarCliente(cliente);
+      onAdicionarCliente(cliente, produtosSelecionados);
       setCliente('');
+      setProdutosSelecionados([]);
     }
   };
 
   const handleClienteChange = (event) => {
     setCliente(event.target.value);
+  };
+
+  const handleProdutoCheckboxChangeInternal = (event, produtoNome) => {
+    const checked = event.target.checked;
+
+    if (checked) {
+      setProdutosSelecionados([...produtosSelecionados, produtoNome]);
+      handleProdutoCheckboxChange(event, produtoNome, cliente);
+    } else {
+      setProdutosSelecionados(produtosSelecionados.filter((produto) => produto !== produtoNome));
+      handleProdutoCheckboxChange(event, produtoNome, cliente);
+    }
   };
 
   return (
@@ -20,6 +34,23 @@ const AdicionarClientes = ({ onAdicionarCliente }) => {
       <div>
         <label htmlFor="nomeCliente">Nome do Cliente:</label>
         <input id="nomeCliente" type="text" value={cliente} onChange={handleClienteChange} />
+      </div>
+
+      <div>
+        <label>Produtos:</label>
+        {produtos.map((produto) => (
+          <div key={produto.nome}>
+            <label>
+              <input
+                type="checkbox"
+                value={produto.nome}
+                checked={produtosSelecionados.includes(produto.nome)}
+                onChange={(event) => handleProdutoCheckboxChangeInternal(event, produto.nome)}
+              />
+              {produto.nome} (R$: {produto.valor})
+            </label>
+          </div>
+        ))}
       </div>
 
       <button onClick={handleAdicionarCliente}>Adicionar Cliente</button>
--- src/pages/Restaurante/Restaurante.jsx
+++ src/pages/Restaurante/Restaurante.jsx
@@ -1,62 +1,129 @@
 import { useState } from 'react';
 import AdicionarClientes from './AdicionarClientes';
-import Produtos from './Produtos';
 
 const Restaurante = () => {
   const [clientes, setClientes] = useState([]);
-  const [clientesSelecionados, setClientesSelecionados] = useState([]);
+  const [produtos, setProdutos] = useState([
+    { nome: 'Pizza', valor: 42.0, consumidores: [] },
+    { nome: 'Refrigerante', valor: 6.0, consumidores: [] },
+    { nome: 'Suco', valor: 7.0, consumidores: [] },
+    { nome: 'Rodízio Simples', valor: 70.0, consumidores: [] },
+    { nome: 'Rodízio Executivo', valor: 85.0, consumidores: [] },
+    { nome: 'Temaki', valor: 20.0, consumidores: [] },
+  ]);
+  const [valorAPagar, setValorAPagar] = useState({});
+  const [taxaServico, setTaxaServico] = useState([]);
 
-  const handleAdicionarCliente = (cliente) => {
-    setClientes([...clientes, cliente]);
+  const handleAdicionarCliente = (cliente, produtosSelecionados) => {
+    setClientes([...clientes, { nome: cliente, produtos: produtosSelecionados }]);
   };
 
-  const handleClienteCheckboxChange = (event) => {
-    const cliente = event.target.value;
+  const handleProdutoCheckboxChange = (event, produtoNome, cliente) => {
     const checked = event.target.checked;
 
-    if (checked) {
-      setClientesSelecionados([...clientesSelecionados, cliente]);
-    } else {
-      setClientesSelecionados(clientesSelecionados.filter((c) => c !== cliente));
-    }
+    setProdutos((prevProdutos) =>
+      prevProdutos.map((produto) => {
+        if (produto.nome === produtoNome) {
+          if (checked) {
+            return { ...produto, consumidores: [...produto.consumidores, cliente] };
+          } else {
+            return {
+              ...produto,
+              consumidores: produto.consumidores.filter((consumidor) => consumidor !== cliente),
+            };
+          }
+        }
+        return produto;
+      })
+    );
   };
 
-  const handleAdicionarConsumo = (produtos) => {
-    const valorPagar = {};
+  const calcularDivisaoConta = (clientes, produtos, taxaServico) => {
+    const resultado = {};
 
-    clientesSelecionados.forEach((cliente) => {
-      const valorTotal = produtos.reduce((acc, produto) => {
+    for (const cliente of clientes) {
+      resultado[cliente] = 0;
+    }
+
+    for (const cliente of clientes) {
+      for (const produto of produtos) {
         if (produto.consumidores.includes(cliente)) {
-          return acc + produto.valor;
+          if (
+            produto.nome === 'Rodízio Simples' ||
+            produto.nome === 'Rodízio Executivo' ||
+            (produto.nome === 'Refrigerante' && produto.consumidores.length === clientes.length)
+          ) {
+            resultado[cliente] += produto.valor;
+          } else if (produto.consumidores.filter((nome) => nome === cliente).length > 1) {
+            let quantidade = produto.consumidores.filter((nome) => nome === cliente).length;
+            resultado[cliente] += produto.valor * quantidade;
+          } else {
+            const quantidade = produto.consumidores.filter((nome) => nome === cliente).length;
+            resultado[cliente] += (produto.valor * quantidade) / produto.consumidores.length;
+          }
         }
-        return acc;
-      }, 0);
+      }
+    }
 
-      valorPagar[cliente] = valorTotal;
-    });
+    for (const cliente of clientes) {
+      const totalGasto = resultado[cliente];
+      if (totalGasto > 0 && taxaServico.includes(cliente)) {
+        resultado[cliente] += totalGasto * 0.1;
+      }
+    }
 
-    console.log('Valor a pagar por cliente:', valorPagar);
+    return resultado;
+  };
+
+  const calcularValorAPagar = () => {
+    const resultado = calcularDivisaoConta(
+      clientes.map((cliente) => cliente.nome),
+      produtos,
+      taxaServico
+    );
+    setValorAPagar(resultado);
   };
 
   return (
     <>
-      <AdicionarClientes onAdicionarCliente={handleAdicionarCliente} />
+      <AdicionarClientes
+        onAdicionarCliente={handleAdicionarCliente}
+        produtos={produtos}
+        handleProdutoCheckboxChange={handleProdutoCheckboxChange}
+      />
+      <h2>Clientes:</h2>
       <ul>
-        {clientes.map((cliente, index) => {
-          return (
-            <li key={index}>
-              <input
-                type="checkbox"
-                value={cliente}
-                checked={clientesSelecionados.includes(cliente)}
-                onChange={handleClienteCheckboxChange}
-              />
-              {cliente}
-            </li>
-          );
-        })}
+        {clientes.map((cliente, index) => (
+          <li key={index}>
+            {cliente.nome}
+            <ul>
+              {cliente.produtos.map((produtoNome) => {
+                const produto = produtos.find((p) => p.nome === produtoNome);
+                return (
+                  <li key={produtoNome}>
+                    {produtoNome}, R$ {produto.valor}
+                  </li>
+                );
+              })}
+            </ul>
+            <p>
+              Total: R${' '}
+              {cliente.produtos.reduce((acc, produtoNome) => {
+                const produto = produtos.find((p) => p.nome === produtoNome);
+                return acc + produto.valor;
+              }, 0)}
+            </p>
+          </li>
+        ))}
       </ul>
-      <Produtos clientes={clientes} adicionarConsumo={handleAdicionarConsumo} />
+      <button onClick={calcularValorAPagar}>Calcular Valor a Pagar</button>
+      <ul>
+        {clientes.map((cliente) => (
+          <li key={cliente.nome}>
+            {cliente.nome} - Valor a Pagar: R$ {valorAPagar[cliente.nome] || 0}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
